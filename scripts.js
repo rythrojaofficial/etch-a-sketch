d6556cc9bc7c1f24c9b463214fd5ae44f62a953b
rythroja <jared@mercenarytricking.com>
2023-09-25 09:15:07 -0700

    add functionality of color picker and grid dimension slider

--- scripts.js
+++ scripts.js
@@ -1,53 +1,72 @@
-// ===================
-// create a looping function that
-// appends a div to a grid at each column 
-// and row specified
-// ====================
-let columns;
+
 let rows;
 let grid;
+// defaults, will change with user input 
 let isMouseHover = false;
 let isMouseDown = false;
-
-gridSketchpad(16,16);
-
-const generateSketchpad = document.getElementById("generate-sketchpad");
-    generateSketchpad.addEventListener('click', () =>{
-        columns = document.getElementById('input-columns').value;
-        rows = document.getElementById('input-rows').value;
-        gridSketchpad(columns,rows);
-    });
+let cellDimension = 24 ;
+let cellColor = '#000000';
+let drawn;
 
 
-function gridSketchpad(numberOfRows,numberOfColumns){
+
+// user inputs 
+let colorPicker = document.getElementById('input-color');
+    colorPicker.oninput = () => {
+        cellColor = document.getElementById('input-color').value;;
+        displayColor = document.querySelector('#color-display');
+        displayColor.textContent = `${cellColor}`;
+};
+
+
+document.body.onmousedown = () => (isMouseDown = true);
+document.body.onmouseup = () => (isMouseDown = false);
+
+
+gridSketchpad(20);
+
+const generateBySlider = document.getElementById('input-dimensions-slider');
+        generateBySlider.oninput = () =>{
+            cellDimension = (480/generateBySlider.value)
+            gridSketchpad(generateBySlider.value);
+            let yxyDisplay = document.querySelector('#yxy-display');
+            yxyDisplay.textContent = `${generateBySlider.value} x ${generateBySlider.value}`;
+            
+
+        }
+       
+        
+
+function gridSketchpad(dimensions){
     grid = document.createElement('div');
     grid.className = 'grid';
     // creates grid of the specified number of columns 
-    for (let i = 0; i < numberOfRows; ++i){
+    for (let i = 0; i < dimensions; ++i){
         let column = document.createElement('div');
             column.className = 'column';
 
         // creates column of the specified number of cells
-        for (let j = 0; j < numberOfColumns; ++j){
+        for (let j = 0; j < dimensions; ++j){
             let cell = document.createElement('div');
                 cell.className = 'cell';
                 cell.textContent = " ";
-                
-                
-                
+                cell.style.height = `${cellDimension}px`;
+                cell.style.width = `${cellDimension}px`;               
                     
 
             column.appendChild(cell);
 
             cell.addEventListener('mouseover', ()=>{
                 if (isMouseDown === true){
-                    cell.className = 'cellDrawn'
+                    cell.className = 'cellDrawn';
+                    cell.style.backgroundColor = `${cellColor}`;
+                    
                 }
-                
             });
             
+            
 
-    }
+        };
         grid.appendChild(column);
         
 
@@ -56,8 +75,7 @@ function gridSketchpad(numberOfRows,numberOfColumns){
     document.getElementById('sketchpad').replaceChildren(grid)
 }
 
-document.body.onmousedown = () => (isMouseDown = true);
-document.body.onmouseup = () => (isMouseDown = false);
+
 
 
 
